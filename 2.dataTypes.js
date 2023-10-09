579be1f758fd80e23826c785baf5f4c678060236
Sadam-Mahar <76719332+Sadam-Mahar@users.noreply.github.com>
2023-10-09 10:29:07 +0000

    datatypes

---
+++ 2.dataTypes.js
@@ -0,0 +1,38 @@
+// "use strict"; // treat all JS code as newer version
+
+// // alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3 
+//     +
+//      3) // code readability should be high
+
+// console.log("Hitesh")
+
+
+// let name = "hitesh"
+// let age = 18
+// let isLoggedIn = false
+// let state;
+
+// // number => 2 to power 53
+// // bigint
+// // string => ""
+// // boolean => true/false
+// // null => standalone value
+// // undefined => 
+// // symbol => unique
+
+
+// // object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+let Name = "sadam"
+let number = 345
+let string = "Sadam Hussain"
+let loginid = false
+let stringtype
+console.log(Name)
+// console.log(typeof number)
+// console.log(typeof string)
+// console.log(typeof loginid)
